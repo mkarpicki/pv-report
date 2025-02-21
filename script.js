baa1abd2d93e8f3d4a4460afa8ca1234a76a7bee
Mariusz <mkarpicki@gmail.com>
2025-02-21 16:26:57 +0100

    Added template for basic info and configured which widgets to load

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
 let mapping = [
-    { selector: "#panel-status-ok", url: "https://thingspeak.com/channels/2814877/widgets/1024018?" },
-    { selector: "#panel-status-alarm", url: "https://thingspeak.com/channels/2814877/widgets/1024019?" },
-    { selector: "#panel-status-fault", url: "https://thingspeak.com/channels/2814877/widgets/1024020?" },
-    { selector: "#panel-status-stand-by", url: "https://thingspeak.com/channels/2814877/widgets/1024021?" },
-    { selector: "#panel-status-self-check", url: "https://thingspeak.com/channels/2814877/widgets/1024022?" }
+    { selector: "#now-status-ok", url: "https://thingspeak.com/channels/2814878/widgets/1026236?" },
+    { selector: "#now-status-alarm", url: "https://thingspeak.com/channels/2814878/widgets/1026237?" },
+    { selector: "#now-status-fault", url: "https://thingspeak.com/channels/2814878/widgets/1026238?" },
+    { selector: "#now-status-stand-by", url: "https://thingspeak.com/channels/2814878/widgets/1026239?" },
+    { selector: "#now-status-self-check", url: "https://thingspeak.com/channels/2814878/widgets/1026240?" },
+
+    { selector: "#now-battery-capacity-clock", url: "https://thingspeak.com/channels/2814869/widgets/1024029?" },
+    { selector: "#now-battery-temperature-value", url: "https://thingspeak.com/channels/2814869/widgets/1024031?" },
+    { selector: "#now-battery-power-value", url: "https://thingspeak.com/channels/2814869/widgets/1024032?" },
+
+    { selector: "#now-instalation-production-power-clock", url: "https://thingspeak.com/channels/2814878/widgets/1024044?" },
+    { selector: "#now-instalation-grid-power-clock", url: "https://thingspeak.com/channels/2814878/widgets/1024045?" },
+    { selector: "#now-instalation-load-power-clock", url: "https://thingspeak.com/channels/2814878/widgets/1024046?" },
+
+    { selector: "#today-buy-value", url: "https://thingspeak.com/channels/2814877/widgets/1024023?" },
+    { selector: "#today-sell-value", url: "https://thingspeak.com/channels/2814877/widgets/1024024?" },
+    { selector: "#today-production-value", url: "https://thingspeak.com/channels/2814877/widgets/1024028?" },
+    { selector: "#today-load-value", url: "https://thingspeak.com/channels/2814877/widgets/1024025?" },
+    { selector: "#today-charge-value", url: "https://thingspeak.com/channels/2814877/widgets/1024026?" },
+    { selector: "#today-discharge-value", url: "https://thingspeak.com/channels/2814877/widgets/1024026?" }
+
 ];
+
+//ajax.json
+//https://thingspeak.mathworks.com/channels/2814878/field/4/last.json
 
 class Monitor {
 
